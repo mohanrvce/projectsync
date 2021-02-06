06e0a82c051a58b1a59bfee293886f71f8bbe484
Mohan <mohanrvce@users.noreply.github.com>
2021-02-06 17:31:06 +0530

    update comments

--- ProjectSync/src/main/java/com/projectsync/testDBConnection/TestSQLDatabaseConnectionSimple.java
+++ ProjectSync/src/main/java/com/projectsync/testDBConnection/TestSQLDatabaseConnectionSimple.java
@@ -25,6 +25,30 @@ public class TestSQLDatabaseConnectionSimple {
 	}
 }
 
+/*
+-- create db: test
+-- create user with username and password: springstudent  
+
+USE [test]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Persons](
+	[ID] [int] NOT NULL,
+	[LastName] [varchar](255) NOT NULL,
+	[FirstName] [varchar](255) NULL,
+	[Age] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
 /*
 package com.projectsync.testDBConnection;
