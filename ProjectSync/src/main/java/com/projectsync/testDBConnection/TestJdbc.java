87e4cd6db4a04f558e6d581f06c1f465d202282c
Mohan <mohanrvce@users.noreply.github.com>
2021-02-06 13:13:39 +0530

    test jdbc connection
    updated code to test id db conenction is successful or not

---
+++ ProjectSync/src/main/java/com/projectsync/testDBConnection/TestJdbc.java
@@ -0,0 +1,21 @@
+package com.projectsync.testDBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "springstudent";
+		String pass = "springstudent";
+
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			System.out.println("Connection successful!!!");
+			System.out.println("Connection Name: " + myConn);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
