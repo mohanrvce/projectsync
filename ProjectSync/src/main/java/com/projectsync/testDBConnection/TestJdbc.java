6ab8ed9f5ffc9018cb90ee3356aa71419aace5f2
Mohan <mohanrvce@users.noreply.github.com>
2021-02-06 16:04:31 +0530

    update jdbc connection string

--- ProjectSync/src/main/java/com/projectsync/testDBConnection/TestJdbc.java
+++ ProjectSync/src/main/java/com/projectsync/testDBConnection/TestJdbc.java
@@ -5,17 +5,20 @@ import java.sql.DriverManager;
 
 public class TestJdbc {
 	public static void main(String[] args) {
-		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		// String jdbcUrl = 
+		// "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String jdbcUrl = "jdbc:mysql://localhost:3306";
 		String user = "springstudent";
 		String pass = "springstudent";
+		// you could also try for user <root> with password <password>
 
 		try {
-			System.out.println("Connecting to database: " + jdbcUrl);
-			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			Connection connection = DriverManager.getConnection(jdbcUrl, user, pass);
 			System.out.println("Connection successful!!");
-			System.out.println("Connection Name: " + myConn);
+			System.out.println("Connection Name: " + connection);
 		} catch (Exception e) {
-			System.out.println("Exception occurred while testing JDBC connection: " + e.getStackTrace());
+			System.out.println("Exception occurred while testing JDBC connection: " + 
+					e.getStackTrace());
 		}
 	}
 }
